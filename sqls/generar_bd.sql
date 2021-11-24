d7e9c1b4a027ddbb480d543e07e7b9b37839e1c2
Gustavo Gutiérrez <gustavo.dgs19@gmail.com>
2021-11-24 19:59:50 -0400

    Correcciones de SQL
    Correcciones de sintaxis en el archivo "generar_bd.sql"

--- sqls/generar_bd.sql
+++ sqls/generar_bd.sql
@@ -1,5 +1,5 @@
 
------------ 18/10/2021 20:00 ------------
+-- --------- 18/10/2021 20:00 ------------
 
 CREATE TABLE `candidatos`  (
   `cedula` varchar(10) NOT NULL,
@@ -39,24 +39,11 @@ CREATE TABLE `se_postulan`  (
   PRIMARY KEY (`candidato`, `empresa`, `vacante`)
 );
 
-CREATE TABLE `table_1`  (
-  `Pk1` integer NOT NULL AUTO_INCREMENT,
-  `prueba` varchar(255) NULL,
-  PRIMARY KEY (`Pk1`)
-);
-
-CREATE TABLE `table_2`  (
-  `Pk2` integer NOT NULL,
-  `loco` varchar(255) NULL,
-  `fk1` integer NULL,
-  PRIMARY KEY (`Pk2`)
-);
-
 CREATE TABLE `vacantes`  (
   `empresa` varchar(30) NOT NULL,
-  `cod_v` integer NOT NULL AUTO_INCREMENT,
+  `cod_v` integer NOT NULL,
   `nombre_v` varchar(255) NOT NULL,
-  `descripcion` varchar NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
   `salario` integer NOT NULL,
   `educacion_min` varchar(255) NOT NULL,
   `edad_min` integer NOT NULL,
@@ -69,6 +56,5 @@ ALTER TABLE `habilidades_c` ADD CONSTRAINT `fk_habilidades_c_Candidatos_1` FOREI
 ALTER TABLE `habilidades_v` ADD CONSTRAINT `fk_habilidades_v_Vacantes_1` FOREIGN KEY (`empresa`, `vacante`) REFERENCES `vacantes` (`empresa`, `cod_v`);
 ALTER TABLE `se_postulan` ADD CONSTRAINT `fk_se_postulan_Candidatos_1` FOREIGN KEY (`candidato`) REFERENCES `candidatos` (`cedula`);
 ALTER TABLE `se_postulan` ADD CONSTRAINT `fk_se_postulan_Vacantes_1` FOREIGN KEY (`empresa`, `vacante`) REFERENCES `vacantes` (`empresa`, `cod_v`);
-ALTER TABLE `table_2` ADD CONSTRAINT `fk_table_2_table_1_1` FOREIGN KEY (`fk1`) REFERENCES `table_1` (`Pk1`);
 ALTER TABLE `vacantes` ADD CONSTRAINT `fk_Vacantes_Empresas_1` FOREIGN KEY (`empresa`) REFERENCES `empresas` (`rif`);
 
