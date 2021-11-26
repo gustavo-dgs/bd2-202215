68f3a7726f0340a1d3bd912a0ee31f2d9ba2f6c2
Gustavo Gutiérrez <gustavo.dgs19@gmail.com>
2021-11-25 23:32:29 -0400

    consulta_4.sql

--- sqls/actualizar_bd.sql
+++ sqls/actualizar_bd.sql
@@ -1,6 +1,17 @@
 
 ALTER TABLE `bd2_202215_27506984`.`vacantes` 
-ADD COLUMN `fecha_apertura` date NULL AFTER `disponibilidad_de_viajar`;
+    ADD COLUMN `fecha_apertura` date NULL AFTER `disponibilidad_de_viajar`;
+ALTER TABLE `bd2_202215_27506984`.`vacantes` 
+    MODIFY COLUMN `fecha_apertura` date NOT NULL AFTER `disponibilidad_de_viajar`;
+
 
 ALTER TABLE `bd2_202215_27506984`.`vacantes` 
-MODIFY COLUMN `fecha_apertura` date NOT NULL AFTER `disponibilidad_de_viajar`;
+    CHANGE COLUMN `fecha_apertura` `fecha_publicacion` date NOT NULL AFTER `disponibilidad_de_viajar`,
+    ADD COLUMN `fecha_vencimiento` date NULL AFTER `fecha_publicacion`,
+    ADD COLUMN `candidato_seleccionado` varchar(10) NULL AFTER `fecha_vencimiento`,
+    ADD CONSTRAINT `fk_Vacantes_Candidatos_1` FOREIGN KEY (`candidato_seleccionado`) 
+        REFERENCES `bd2_202215_27506984`.`candidatos` (`cedula`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `bd2_202215_27506984`.`vacantes` 
+    MODIFY COLUMN `fecha_vencimiento` date NOT NULL AFTER `fecha_publicacion`;
+
+
--- sqls/consulta_3.sql
+++ sqls/consulta_3.sql
@@ -2,15 +2,18 @@
 -- en la que el postulante y el puesto coincidan en, al menos, 
 -- tres habilidades.
 
-SELECT sp.empresa, e.nombre, sp.vacante, sp.candidato, c.nombre
+SELECT e.nombre AS empresa, sp.vacante AS codigo_vacante,
+				v.nombre_v AS vacante, c.nombre AS candidato
 FROM se_postulan sp, habilidades_c hc,
-    candidatos c, vacantes v, empresas e
+		 vacantes v, empresas e, candidatos c
 WHERE sp.candidato = hc.candidato
-AND e.rif = sp.empresa
+AND v.empresa = sp.empresa
+AND v.cod_v = sp.vacante
+AND e.rif = v.empresa
 AND c.cedula = sp.candidato
 AND hc.habilidad IN (SELECT hv.habilidad
                         FROM habilidades_v hv
                         WHERE hv.empresa = sp.empresa
                         AND hv.vacante = sp.vacante)
-GROUP BY 1,2,3
+GROUP BY 1,2,3,4
 HAVING COUNT(hc.habilidad) > 3;
